effe48dfd8f57e89caf166639a1dbc9900de7af1
pkiop <pkiop@naver.com>
2020-10-27 22:38:54 +0900

    feat: 로그인 기능으로 Redux 사용

---
+++ zerocho-typescript-react-webgame/Redux/App.tsx
@@ -0,0 +1,21 @@
+import * as React from 'react';
+import { Component } from 'react';
+import { connect } from 'react-redux';
+
+class App extends Component {
+  render() {
+    const { user } = this.props;
+    return (
+      <div>
+        {user.isLoggingIn
+          ? <div>로그인 중</div>
+          : user.data
+            ? <div>{user.data.nickname}</div>
+            : '로그인 해주세요'}
+        {!user.data
+          ? <button onClick={this.onClick}>로그인</button>
+          : <button onClick={this.onLogout}>로그아웃</button>}
+      </div>
+    )
+  }
+}
