26fa1ff1b08247e0a955ad699884c95a5b767808
pkiop <pkiop@naver.com>
2020-10-27 23:00:32 +0900

    feat: redux에서 user의 초기정보와 userReducer를 담는 user.ts

---
+++ zerocho-typescript-react-webgame/Redux/reducers/user.ts
@@ -0,0 +1,13 @@
+const initialState = {
+  isLoggingIn: false,
+  data: null,
+};
+
+const userReducer = (prevState, action) => {
+  switch (action.type) {
+    default:
+      return prevState;
+  }
+}
+
+export default userReducer;
