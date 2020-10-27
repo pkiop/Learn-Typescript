97d50f1ec26fbb9d14c368fbb3553e3830a39afd
pkiop <pkiop@naver.com>
2020-10-28 00:31:18 +0900

    feat: user Reducer 작성

--- zerocho-typescript-react-webgame/Redux/reducers/user.ts
+++ zerocho-typescript-react-webgame/Redux/reducers/user.ts
@@ -1,10 +1,26 @@
+import { LOG_IN_REQUEST, LOG_IN_FAILURE, LOG_IN_SUCCESS,
+        LogInFailureAction, LogInRequestAction, LogInSuccessAction, LogOutAction, LOG_OUT
+} from '../actions/user';
+
+export interface UserState {
+  isLoggingIn: boolean,
+  data: {
+    nickname: string,
+  } | null
+}
+
 const initialState = {
   isLoggingIn: false,
   data: null,
 };
 
-const userReducer = (prevState, action) => {
+type UserReducerActions = LogInFailureAction | LogInRequestAction | LogInSuccessAction | LogOutAction;
+const userReducer = (prevState = initialState, action: UserReducerActions) => {
   switch (action.type) {
+    case LOG_IN_REQUEST:
+    case LOG_IN_SUCCESS:
+    case LOG_IN_FAILURE:
+    case LOG_OUT:
     default:
       return prevState;
   }
