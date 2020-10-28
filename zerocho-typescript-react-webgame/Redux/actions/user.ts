1523407df9975fb37df39a3e46dedf44572dda17
pkiop <pkiop@naver.com>
2020-10-28 13:22:30 +0900

    feat: user action 함수 정의

--- zerocho-typescript-react-webgame/Redux/actions/user.ts
+++ zerocho-typescript-react-webgame/Redux/actions/user.ts
@@ -1,3 +1,6 @@
+import { addPost } from './post';
+import { AnyAction} from "redux";
+
 export const LOG_IN_REQUEST = 'LOG_IN_REQUEST' as const;
 export const LOG_IN_SUCCESS = 'LOG_IN_SUCCESS' as const;
 export const LOG_IN_FAILURE = 'LOG_IN_FAILURE' as const;
@@ -8,9 +11,23 @@ export interface LogInRequestAction {
   data: {id: string, password: string},
 }
 
+export const logInRequest = (data: {id: string, password: string}): LogInRequestAction => {
+  return {
+    type: LOG_IN_REQUEST,
+    data,
+  }
+}
+
 export interface LogInSuccessAction {
   type: typeof LOG_IN_SUCCESS,
-  data: {userId: string, nickname: string},
+  data: {userId: number, nickname: string},
+}
+
+export const logInSuccess = (data: {userId: number, nickname: string}): LogInSuccessAction => {
+  return {
+    type: LOG_IN_SUCCESS,
+    data,
+  }
 }
 
 export interface LogInFailureAction {
@@ -18,8 +35,37 @@ export interface LogInFailureAction {
   error: Error,
 }
 
-export const logIn = (data: {id: string, password: string}) => {
+export const logInFailure = (error: Error): LogInFailureAction => {
+  return {
+    type: LOG_IN_FAILURE,
+    error,
+  }
+}
 
+export interface ThunkDispatch {
+  (thunkAction: ThunkAction): void,
+  <A>(action: A): AnyAction,
+  <TAction>(action: TAction | ThunkAction): TAction,
+}
+
+
+type ThunkAction = (dispatch: ThunkDispatch) => void;
+
+export const logIn = (data: {id: string, password: string}): ThunkAction => {
+  return (dispatch) => {
+    dispatch(logInRequest(data));
+    try {
+      setTimeout(() => {
+        dispatch(logInSuccess({
+          userId: 1,
+          nickname: 'zerocho',
+        }));
+        dispatch(addPost);
+      }, 1000)
+    } catch (err) {
+      dispatch(logInFailure(err));
+    }
+  }
 }
 
 export interface LogOutAction {
