af88cc7eeff4568c808010fb9a4a0ce290e36c9d
pkiop <pkiop@naver.com>
2020-10-28 13:21:24 +0900

    feat: Hook 버전 App으로 변경

--- zerocho-typescript-react-webgame/Redux/App.tsx
+++ zerocho-typescript-react-webgame/Redux/App.tsx
@@ -1,58 +1,40 @@
 import * as React from 'react';
-import { Component } from 'react';
-import { Dispatch } from 'redux'; // react에도 있긴 함
-import { connect } from 'react-redux';
-import { logIn, logOut } from './actions/user';
+import { Component, FC } from 'react';
+import { connect,useSelector, useDispatch } from 'react-redux';
+import { logIn, logOut, ThunkDispatch } from './actions/user';
 import { RootState } from './reducers';
 import { UserState } from './reducers/user';
 
-interface StateToProps {
-  user: UserState,
-}
 
-interface DispatchToProps {
-  dispatchLogIn: ({id, password}: {id: string, password: string}) => void,
-  dispatchLogOut: () => void,
-}
 
-class App extends Component<StateToProps & DispatchToProps> {
 
-  onClick = () => {
-    this.props.dispatchLogIn({
+const App: FC = () => {
+  const { isLoggingIn, data} = useSelector<RootState, UserState>((state) => state.user);
+  const dispatch = useDispatch();
+
+  const onClick = () => {
+    dispatch(logIn({
       id: 'zerocho',
       password: '비밀번호',
-    })
+    }));
   }
 
-  onLogout = () => {
-    this.props.dispatchLogOut();
+  const onLogout = () => {
+    dispatch(logOut());
   }
-
-
-  render() {
-    const { user } = this.props;
     return (
       <div>
-        {user.isLoggingIn
+        {isLoggingIn
           ? <div>로그인 중</div>
-          : user.data
-            ? <div>{user.data.nickname}</div>
+          : data
+            ? <div>{data.nickname}</div>
             : '로그인 해주세요'}
-        {!user.data
-          ? <button onClick={this.onClick}>로그인</button>
-          : <button onClick={this.onLogout}>로그아웃</button>}
+        {!data
+          ? <button onClick={onClick}>로그인</button>
+          : <button onClick={onLogout}>로그아웃</button>}
       </div>
     )
-  }
-}
 
-const mapStateToProps = (state: RootState) => ({
-  user: state.user,
-  posts: state.posts,
-}); // reselect 쓸 분은 쓰세요
+};
 
-const mapDispatchToProps = (dispatch: Dispatch) => ({
-  dispatchLogin: (data: {id: string, password: string}) => dispatch(logIn(data)),
-  dispatchLogOut: () => dispatch(logOut()),
-})
-export default connect(mapStateToProps, mapDispatchToProps)(App);
+export default App;
